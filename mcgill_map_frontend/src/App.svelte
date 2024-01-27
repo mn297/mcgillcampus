564e6cc2f2c2299a02a30296191225d87f0f98f0
Your Name <peterwayne3293@gmail.com>
2024-01-27 14:45:44 -0500

    env change

--- mcgill_map_frontend/src/App.svelte
+++ mcgill_map_frontend/src/App.svelte
@@ -1,13 +1,15 @@
 <script>
   import Map from "./Map.svelte";
   export let ready;
+
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 </script>
 
 <svelte:head>
   <script
     defer
     async
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCALH7OHHPmgyR8m4dW3j7mJf8PZTfJZD4&callback=initMap"
+    src={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`}
   >
   </script>
 </svelte:head>
