07c75db24d50c089b5d5f01243f4a95c976b11c4
Your Name <peterwayne3293@gmail.com>
2024-01-27 16:29:31 -0500

    dropdown

--- mcgill_map_frontend/src/Map.svelte
+++ mcgill_map_frontend/src/Map.svelte
@@ -40,7 +40,6 @@
 				console.error("Error:", error);
 			}
 
-			// test data
 			// Fetch course data
 			try {
 				const response = await fetch(
@@ -73,6 +72,17 @@
 		// Future implementation: Use searchQuery to perform a search
 	}
 </script>
+<div class="date-selector">
+    <select>
+        <option value="Monday">Monday</option>
+        <option value="Tuesday">Tuesday</option>
+        <option value="Wednesday">Wednesday</option>
+        <option value="Thursday">Thursday</option>
+        <option value="Friday">Friday</option>
+        <option value="Saturday">Saturday</option>
+        <option value="Sunday">Sunday</option>
+    </select>
+</div>
 
 <div class="search-container">
 	<input
